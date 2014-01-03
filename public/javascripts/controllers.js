c48cf0817582fa977eb7fff38be8c262e689f972
Jennifer Fong <jen@ednapiranha.com>
2014-01-02 18:22:15 -0800

    removing unecessary profile call

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -392,52 +392,44 @@ angular.module('chatspace.controllers', []).
   controller('DashboardCtrl', function ($scope, $rootScope, $http, $location, $routeParams, api) {
     api.call();
 
-    $http({
-      url: '/api/profile',
-      method: 'GET'
-    }).success(function (data) {
-      $rootScope.userHash = data.userHash;
-      $rootScope.recipients = {};
-      $rootScope.messages = {};
-      $scope.isLoading = true;
+    $scope.isLoading = true;
 
-      var since = '';
+    var since = '';
 
-      // load all the messages from the local cache
-      localForage.getItem($rootScope.userHash + ':dashboardList', function (data) {
-        if (data) {
-          $rootScope.dashboardList = data;
-        }
+    // load all the messages from the local cache
+    localForage.getItem($rootScope.userHash + ':dashboardList', function (data) {
+      if (data) {
+        $rootScope.dashboardList = data;
+      }
 
-        $rootScope.dashboardList.forEach(function (d) {
-          // remove any occurences of your own userHash and add the final message to the messages scope
-          localForage.getItem($rootScope.userHash + ':dashMessage[' + d + ']', function (thread) {
-            thread.value.recipients.forEach(function (r, idx) {
-              if (r === $rootScope.userHash) {
-                thread.value.recipients.splice(idx, 1);
-              }
-            });
-
-            $rootScope.recipientAvatars = thread.value.recipientAvatars;
-
-            if ($rootScope.recipientAvatars) {
-           //   $rootScope.recipientAvatars.splice($rootScope.recipientAvatars.indexOf($rootScope.avatar), 1);
+      $rootScope.dashboardList.forEach(function (d) {
+        // remove any occurences of your own userHash and add the final message to the messages scope
+        localForage.getItem($rootScope.userHash + ':dashMessage[' + d + ']', function (thread) {
+          thread.value.recipients.forEach(function (r, idx) {
+            if (r === $rootScope.userHash) {
+              thread.value.recipients.splice(idx, 1);
             }
-
-            $rootScope.messages[d] = thread;
           });
-        });
 
-        if ($rootScope.dashboardList[0]) {
-          since = '?since=' + $rootScope.dashboardList[0];
-        }
+          $rootScope.recipientAvatars = thread.value.recipientAvatars;
 
-        $http({
-          url: '/api/feed' + since,
-          method: 'GET'
-        }).success(function (data) {
-          $scope.isLoading = false;
+          if ($rootScope.recipientAvatars) {
+         //   $rootScope.recipientAvatars.splice($rootScope.recipientAvatars.indexOf($rootScope.avatar), 1);
+          }
+
+          $rootScope.messages[d] = thread;
         });
+      });
+
+      if ($rootScope.dashboardList[0]) {
+        since = '?since=' + $rootScope.dashboardList[0];
+      }
+
+      $http({
+        url: '/api/feed' + since,
+        method: 'GET'
+      }).success(function (data) {
+        $scope.isLoading = false;
       });
     });
 
